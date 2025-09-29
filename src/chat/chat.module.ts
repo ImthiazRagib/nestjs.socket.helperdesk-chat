e7784a98617f778d97ab8fe092a086f8f69b15eb
imthiazSB <imtiaz@softbuilders.ae>
2025-09-29 12:31:00 +0400

    refactor(chat): add RoomsService and UsersService to providers
    Inject RoomsService and UsersService as dependencies to enable cross-module functionality

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -4,10 +4,12 @@ import { ChatGateway } from './chat/chat.gateway';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ChatMessage } from './entity/chat-message.entity';
 import { RoomsModule } from 'src/rooms/rooms.module';
+import { RoomsService } from 'src/rooms/rooms.service';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [SequelizeModule.forFeature([ChatMessage]), RoomsModule],
-  providers: [ChatService, ChatGateway],
+  providers: [ChatService, ChatGateway, RoomsService, UsersService],
   exports: [ChatService]
 })
-export class ChatModule {}
+export class ChatModule { }
